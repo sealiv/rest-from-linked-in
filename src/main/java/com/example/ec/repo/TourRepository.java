210def3d959af557f3a13e5f8f01c6997e5539c5
Sealiv <sai.79.ua@gmail.com>
2022-06-07 12:36:14 +0300

    3-3 : Explore the /search resource

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -2,6 +2,9 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 /**
@@ -10,4 +13,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Mary Ellen Bowman
  */
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(@Param("code") String code);
 }
