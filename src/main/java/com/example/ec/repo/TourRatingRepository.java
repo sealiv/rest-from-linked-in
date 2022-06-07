669286b2672ccdca7ace02a31bea454186e2a373
Sealiv <sai.79.ua@gmail.com>
2022-06-07 15:27:19 +0300

    4-6 : Paging and Sorting DTOs

--- src/main/java/com/example/ec/repo/TourRatingRepository.java
+++ src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -2,7 +2,10 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.TourRating;
 import com.example.ec.domain.TourRatingPk;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -14,7 +17,7 @@ import java.util.Optional;
  * Created by Mary Ellen Bowman
  */
 @RepositoryRestResource(exported = false)
-public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+public interface TourRatingRepository extends PagingAndSortingRepository<TourRating, TourRatingPk> {
 
     /**
      * Lookup all the TourRatings for a tour.
@@ -31,4 +34,6 @@ public interface TourRatingRepository extends CrudRepository<TourRating, TourRat
      * @return Optional of found TourRatings.
      */
     Optional<TourRating> findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+
+    Page<TourRating> findByPkTourId(Integer tourId, Pageable pageable);
 }
